4215f3f1e2df7e9992c500357a1d48435f3035de
Filip Nedeljković <nedeljkovic.filip.dev@gmail.com>
2024-01-28 20:39:34 +0100

    Added tailwind, login section, and dashboard page

--- reactjs-management/src/App.js
+++ reactjs-management/src/App.js
@@ -1,34 +1,17 @@
 import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import { Routes, Route } from "react-router-dom"
+import Dashboard from './components/Dashboard/main';
+import Login from './components/Login/main';
+
 
 function App() {
-  
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getAllUsers()
-  }, []);
-
-  const getAllUsers = () => {
-    axios.get('http://localhost:4000/users/getAll')
-    .then(response => {
-      setData(response.data)
-    })
-    .catch(error => console.error(error))
-  }
 
   return (
-    <div>
-      <h1>Connection MERN</h1>
-      {
-        data.length && (
-          <ul>
-            {data.map(user => (
-              <li key={user._id}>{user.name}, {user.age}</li>
-            ))}
-        </ul>
-        )
-      }
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={ <Dashboard /> } />
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
     </div>
   )
 }
